b92c53194a2f1134cf016798c2beeeb47a56d87c
Kushan Fernando <Kushan.Fernando@trustwave.com>
2025-12-07 01:11:21 +1300

    initial av project

---
+++ src/TritonSim.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TritonSim.GUI.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ src/TritonSim.GUI/Views/SimulationWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TritonSim.GUI.Views;
+
+public partial class SimulationWindow : Window
+{
+    public SimulationWindow()
+    {
+        InitializeComponent();
+    }
+}
